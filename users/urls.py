6027942c5b8f0572a4be6d6ba3279a537b1c497c
Huzaifa Qamer <huzaifaqamer@gmail.com>
2020-02-25 18:33:27 +0500

    user logout view

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.UserRegisterView.as_view(), name='user-register'),
     path('login', obtain_auth_token, name='user-login'),
+    path('logout', views.UserLogoutView.as_view(), name='user-logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,21 @@
 from django.contrib.auth.models import User
 
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView
 
 from . import serializers
+
 
 class UserRegisterView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = serializers.UserRegisterSerializer
 
+
+class UserLogoutView(DestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        token = self.request.user.auth_token
+        return token
