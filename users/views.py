47fa43c5e143fba48c535574ae03202cc46183b5
Huzaifa Qamer <huzaifaqamer@gmail.com>
2020-02-25 19:10:00 +0500

    removed unrequired attributes in classes

--- users/views.py
+++ users/views.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.models import User
-
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView, DestroyAPIView
 
@@ -7,13 +5,11 @@ from . import serializers
 
 
 class UserRegisterView(CreateAPIView):
-    queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = serializers.UserRegisterSerializer
 
 
 class UserLogoutView(DestroyAPIView):
-    queryset = User.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_object(self):
