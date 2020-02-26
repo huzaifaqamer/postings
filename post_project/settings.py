0ee47f6ab2e6840598d7bce5c6fb501d95d09a4f
Huzaifa Qamer <huzaifaqamer@gmail.com>
2020-02-26 19:34:21 +0500

    added posts app

--- post_project/settings.py
+++ post_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'users',
+    'posts',
 ]
 
 MIDDLEWARE = [
--- post_project/urls.py
+++ post_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
