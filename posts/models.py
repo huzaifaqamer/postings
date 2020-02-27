c58dfa5e390ee5f11eaf31561f935e1ff81cc9c2
Huzaifa Qamer <huzaifaqamer@gmail.com>
2020-02-27 14:42:10 +0500

    Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    class PostStatus(models.TextChoices):
+        DRAFT = 'D', 'Draft'
+        UNPUBLISHED = 'U', 'Unpublished'
+        PUBLISHED = 'P', 'Published'
+    
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=1000)
+    status = models.CharField(
+        max_length=1,
+        choices=PostStatus.choices,
+        default=PostStatus.DRAFT
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
